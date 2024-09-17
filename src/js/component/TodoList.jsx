d8504f40d96532ccf3550301047797487e1a6c0e
nob86 <gustavoadallacroce@gmail.com>
2024-09-17 11:21:25 +0000

    todolisto

---
+++ src/js/component/TodoList.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const TodoList = () => {
+  
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className="col">
+      <input type='text' />
+      </div>
+      </div>
+      <ul>
+        <li>primera tarea</li>
+        <li>segunda tarea</li>
+      </ul>
+      
+    </div>
+  )
+}
+
+export default TodoList
